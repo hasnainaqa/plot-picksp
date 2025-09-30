648eb94c84fd163c9011bf286a0154e43913d678
hasnainaqa <muhammedhasnain033@gmail.com>
2025-09-30 14:44:03 +0500

    btn animation

--- src/Components/LandingPage/ui/Button.tsx
+++ src/Components/LandingPage/ui/Button.tsx
@@ -11,24 +11,30 @@ const Button: React.FC<ButtonProps> = ({
   ...props
 }) => {
   return (
-    <div className="relative inline-block rounded-full p-[2px] overflow-hidden">
-      {/* Spinning gradient border */}
+    <div className="relative inline-block rounded-full p-[1px] overflow-hidden group">
+      {/* Spinning purple gradient border (hidden on hover) */}
       <div
         className="
           absolute inset-0 rounded-full
           bg-[conic-gradient(from_0deg,#8E24AA_-35.11%,#000000_82.19%)]
           animate-spin-slow
+          group-hover:hidden
         "
       ></div>
 
-      {/* Gray static base border (D8D8D8) */}
-      <div className="absolute inset-0 rounded-full border border-[#D8D8D8]"></div>
+      {/* Gray border (visible only on hover) */}
+      <div
+        className="
+          absolute inset-0 rounded-full border border-[#D8D8D8]
+          hidden group-hover:block
+        "
+      ></div>
 
       {/* Button itself */}
       <button
         className={`
           relative z-10 px-10 py-3 font-semibold text-white rounded-full 
-          bg-black hover:bg-gray-900 transition-colors duration-300
+          bg-black transition-colors duration-300
           ${className}
         `}
         {...props}
